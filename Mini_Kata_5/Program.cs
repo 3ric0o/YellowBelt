513113fdfd3d841d53e3270813cc015f8e6ef991
Aergu <erik.witkowski@student.forsbergsskola.se>
2024-11-04 12:13:28 +0100

    YellowBelt

---
+++ Mini_Kata_5/Program.cs
@@ -0,0 +1,21 @@
+﻿
+namespace YellowBelt;
+
+class Program
+{
+    static string player = "Player";
+    static void Main(string[] args)
+    {
+        Attack(15);
+        Heal(10);
+    }
+    static void Attack(int damage)
+    {
+        Console.WriteLine($"{player} dealt {damage} damage!");
+    }
+
+    static void Heal(int healAmount)
+    {
+        Console.WriteLine($"{player} healed {healAmount} health!");
+    }
+}
