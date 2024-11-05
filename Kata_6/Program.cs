086819c1943353dc6e89c14ea98eb1d7adf61dad
Aergu <erik.witkowski@student.forsbergsskola.se>
2024-11-05 09:54:48 +0100

    kata 6

---
+++ Kata_6/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace Kata_6;
+
+class Program
+{
+    static string[] enemyNames = { "Goblin", "Orc", "Troll", "Skeleton", "Dragon" };
+    static List<string> playerInventory = new List<string> {"Sword", "Shield", "Potion"};
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Enemies:");
+        foreach (string enemy in enemyNames)
+        {
+            Console.WriteLine(enemy);
+        }
+        Console.WriteLine("\nPlayer Inventory:");
+        playerInventory.ForEach(Console.WriteLine);
+        
+        playerInventory.Add("Helmet");
+        playerInventory.Add("Armor");
+        playerInventory.Remove("Potion");
+        
+        Console.WriteLine("\nUpdated Inventory:");
+        playerInventory.ForEach(Console.WriteLine);
+    }
+}
