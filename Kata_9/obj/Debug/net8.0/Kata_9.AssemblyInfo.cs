140ad01c6942178d7a385509a4d548acfdc8998b
Aergu <erik.witkowski@student.forsbergsskola.se>
2024-11-06 09:37:53 +0100

    add

--- Kata_9/obj/Debug/net8.0/Kata_9.AssemblyInfo.cs
+++ Kata_9/obj/Debug/net8.0/Kata_9.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Kata_9")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0c235143f3c842f207419cfb7c71adf467ddd760")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+22ee7204b881ee8e1986ddcd7876b995f8fe93ac")]
 [assembly: System.Reflection.AssemblyProductAttribute("Kata_9")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Kata_9")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
